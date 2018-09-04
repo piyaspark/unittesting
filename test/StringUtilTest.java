557f7d59661c67360d195d8513d69dd3e2180e78
Piyaphol Wiengperm <piyaphol.w@ku.th>
2018-09-04 13:54:33 +0700

    improved and tested StringUtil

--- test/StringUtilTest.java
+++ test/StringUtilTest.java
@@ -12,11 +12,44 @@ public class StringUtilTest {
 	public void setUp() throws Exception {
 	}
 
+	/**
+	 * Test that the method work correctly.
+	 */
 	@Test
 	public void testIndexOf() {
 		String[] fruit = {"Apple", "Banana", "Grapes"};
 		assertEquals(0, StringUtil.indexOf("Apple", fruit));
 	}
 
-	//TODO write more tests. Test other cases.
+	/**
+	 * Test that the method always return the index of first occurrence of arg.
+	 */
+	@Test
+	public void testMultipleArg() {
+		String[] fruit = {"Apple", "Apple", "Grapes"};
+		assertEquals(0, StringUtil.indexOf("Apple", fruit));
+		String[] fruit2 = {"Banana", "Apple", "Apple"};
+		assertEquals(1,StringUtil.indexOf("Apple", fruit2));
+	}
+
+	/**
+	 * The method will return -1 if arg is not in the array.
+	 */
+	@Test
+	public void testNoArgInArray(){
+		String[] fruit = {"Apple", "Banana", "Grapes"};
+		assertEquals(-1,StringUtil.indexOf("Potato", fruit));
+	}
+
+	/**
+	 * Test the method that can work correctly even arg is uppercase or lowercase.
+	 */
+	@Test
+	public void testArgIgnoreCase(){
+		String[] fruit = {"aPplE", "BaNaNa", "GRAPES"};
+		assertEquals(0,StringUtil.indexOf("apple", fruit));
+		assertEquals(1,StringUtil.indexOf("banana", fruit));
+		assertEquals(2,StringUtil.indexOf("grapes", fruit));
+	}
+
 }
